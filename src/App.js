0e6fdb664ff07e8a7fbc55f95aecde21fbe46d64
Vchuks <64593598+Vchuks@users.noreply.github.com>
2022-11-04 16:50:58 +0100

    linked the home page with page name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Contact from "./Contact";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home/>}/>
+      <Route path="/userprofile" element={<Home/>}/>
     <Route path="contact" element={<Contact />} />
   </Routes>
   );
