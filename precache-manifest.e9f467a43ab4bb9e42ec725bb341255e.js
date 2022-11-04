d4e8cb02d6d45d94b60d793d3ae8704b236843a9
Vchuks <64593598+Vchuks@users.noreply.github.com>
2022-11-04 16:35:16 +0100

    Updates

---
+++ precache-manifest.e9f467a43ab4bb9e42ec725bb341255e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4db2ee0872b000503bc4",
+    "url": "/hng9/static/css/main.347d1e6c.chunk.css"
+  },
+  {
+    "revision": "4db2ee0872b000503bc4",
+    "url": "/hng9/static/js/main.4db2ee08.chunk.js"
+  },
+  {
+    "revision": "dcb7df9bbfde9dd1c6b0",
+    "url": "/hng9/static/js/1.dcb7df9b.chunk.js"
+  },
+  {
+    "revision": "177add82ae698aee87d8",
+    "url": "/hng9/static/js/2.177add82.chunk.js"
+  },
+  {
+    "revision": "ebd4e0428ed079b10816",
+    "url": "/hng9/static/js/runtime~main.ebd4e042.js"
+  },
+  {
+    "revision": "7e31ec7f651be70e5188f9d5ec324f86",
+    "url": "/hng9/static/media/avatar.7e31ec7f.png"
+  },
+  {
+    "revision": "4b7b982985f6ba6679dd05d0a4fe3e7f",
+    "url": "/hng9/index.html"
+  }
+];
